079efc1e650676fd5e4788cf9a6a0dbce5a615b8
Gábor Görzsöny <gabor@gorzsony.com>
2014-12-30 06:10:55 +0100

    Added headers to source files

--- include/gg/timer.hpp
+++ include/gg/timer.hpp
@@ -1,3 +1,11 @@
+/**
+* Copyright (c) 2014-2015 Gábor Görzsöny (www.gorzsony.com)
+*
+* This source is a private work and can be used only with the
+* written permission of the author. Do not redistribute it!
+* All rights reserved.
+*/
+
 #ifndef GG_TIMER_HPP_INCLUDED
 #define GG_TIMER_HPP_INCLUDED
 
