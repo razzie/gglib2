6ca5967f8ea11f63cdd1b710df9ae86613fca29c
Gábor Görzsöny <gabor@gorzsony.com>
2015-01-06 02:10:56 +0100

    FastMutex

---
+++ include/gg/fastmutex.hpp
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2014-2015 Gábor Görzsöny (www.gorzsony.com)
+ *
+ * This source is a private work and can be used only with the
+ * written permission of the author. Do not redistribute it!
+ * All rights reserved.
+ */
+
+#ifndef GG_FASTMUTEX_HPP_INCLUDED
+#define GG_FASTMUTEX_HPP_INCLUDED
+
+#include <atomic>
+#include <mutex>
+#include <thread>
+
+namespace gg
+{
+	class FastMutex
+	{
+	private:
+		std::atomic_flag m_lock;
+
+	public:
+		FastMutex()
+		{
+			m_lock.clear();
+		}
+
+		FastMutex(const FastMutex&) = delete;
+
+		void lock()
+		{
+			while (m_lock.test_and_set())
+			{
+				std::this_thread::yield();
+			}
+		}
+
+		void unlock()
+		{
+			m_lock.clear();
+		}
+	};
+};
+
+#endif // GG_FASTMUTEX_HPP_INCLUDED
