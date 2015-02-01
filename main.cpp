e9a1951a83ab5bb83434b3dad92152f8b3597551
Gábor Görzsöny <gabor@gorzsony.com>
2015-02-01 02:43:44 +0100

    Removed AntTweakBar library

--- main.cpp
+++ main.cpp
@@ -13,15 +13,8 @@
 #include "expression.hpp"
 #include "gg/console.hpp"
 
-#define D3D_DEBUG_INFO
-#include <d3d9.h>
-#pragma comment(lib, "d3d9.lib")
-
 int main()
 {
-	gg::console.init();
-
-	IDirect3D9* pD3D = Direct3DCreate9(D3D_SDK_VERSION);
 
 	return 0;
 }
