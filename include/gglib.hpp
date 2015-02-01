bd260c0415a25087dbf98b038f02164b9cef6ca2
Gábor Görzsöny <gabor@gorzsony.com>
2015-02-01 05:01:42 +0100

    Added gglib.hpp

---
+++ include/gglib.hpp
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2014-2015 Gábor Görzsöny (www.gorzsony.com)
+ *
+ * This source is a private work and can be used only with the
+ * written permission of the author. Do not redistribute it!
+ * All rights reserved.
+ */
+
+#ifndef GGLIB_HPP_INCLUDED
+#define GGLIB_HPP_INCLUDED
+
+#include "gg/config.hpp" // should be always the first
+
+#include "gg/console.hpp"
+#include "gg/fastmutex.hpp"
+#include "gg/function.hpp"
+#include "gg/logger.hpp"
+#include "gg/message.hpp"
+#include "gg/serializer.hpp"
+#include "gg/storage.hpp"
+#include "gg/streamutil.hpp"
+#include "gg/timer.hpp"
+#include "gg/var.hpp"
+
+#endif // GGLIB_HPP_INCLUDED
--- main.cpp
+++ main.cpp
@@ -1,17 +1,5 @@
 #include <iostream>
-#include "gg/fastmutex.hpp"
-#include "gg/streamutil.hpp"
-#include "gg/var.hpp"
-#include "gg/function.hpp"
-#include "gg/timer.hpp"
-#include "gg/storage.hpp"
-#include "gg/serializer.hpp"
-#include "buffer.hpp"
-#include "gg/filesystem.hpp"
-#include "gg/logger.hpp"
-#include "gg/message.hpp"
-#include "expression.hpp"
-#include "gg/console.hpp"
+#include "gglib.hpp"
 
 int main()
 {
