48db782c02539b8085c1af81a31820699f6604a5
Gábor Görzsöny <gabor@gorzsony.com>
2015-07-29 03:44:33 +0200

    Reduced IDGenerator range

--- include/gg/idgenerator.hpp
+++ include/gg/idgenerator.hpp
@@ -31,28 +31,31 @@ namespace gg
 			std::mt19937 mt(rd());
 			std::uniform_int_distribution<uint16_t> dist(0, MAX_VALUE);
 
-			const uint8_t a = (dist(mt) % 163) + 1;
-			const uint8_t b = (dist(mt) % 131) + 1;
-			const uint8_t c = (dist(mt) % 101) + 1;
+			do
+			{
+				const uint8_t a = (dist(mt) % 163) + 1;
+				const uint8_t b = (dist(mt) % 131) + 1;
+				const uint8_t c = (dist(mt) % 101) + 1;
 
-			m_step = (a * MAX_VALUE * MAX_VALUE) + (b * MAX_VALUE) + c;
-			m_step &= ~0xc0000000;
-			m_step %= PRIME;
+				m_step = (a * MAX_VALUE * MAX_VALUE) + (b * MAX_VALUE) + c;
+				m_step &= ~0xc0000000;
+				m_step %= PRIME;
+			} while (m_step == 0);
 		}
 
 		ID next()
 		{
 			uint32_t next = m_current.fetch_add(m_step) + m_step;
-			if (next > ~0xc0000000)
-				m_current.fetch_sub(~0xc0000000);
+			if (next > 0x00ffffff)
+				m_current.fetch_sub(0x00ffffff);
 
 			next %= PRIME;
 			return static_cast<uint16_t>(next);
 		}
 
 	private:
-		const uint16_t MAX_VALUE = ~0;
-		const uint32_t PRIME = 65537;
+		const uint16_t MAX_VALUE = 21001;
+		const uint32_t PRIME = 21001;
 
 		uint32_t m_step;
 		std::atomic<uint32_t> m_current;
