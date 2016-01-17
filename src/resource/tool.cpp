4ed3681a63d204518a0281952be437f71760058b
Gábor Görzsöny <gabor@gorzsony.com>
2016-01-17 14:55:11 +0100

    Updated year to 2016 in file headers

--- src/resource/tool.cpp
+++ src/resource/tool.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2014-2015 Gábor Görzsöny (www.gorzsony.com)
+ * Copyright (c) 2014-2016 Gábor Görzsöny (www.gorzsony.com)
  *
  * This source is a private work and can be used only with the
  * written permission of the author. Do not redistribute it!
